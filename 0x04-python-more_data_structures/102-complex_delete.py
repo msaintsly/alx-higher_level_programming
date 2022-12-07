e084c752e81b1717f96c4f7e0aa81a2ec9392a81
Musa Sylvester Echi <msaintsly@gmail.com>
2022-12-07 13:19:10 -0800

    task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key in list(a_dictionary):
-        if a_dictionary[key] == value:
-            del a_dictionary[key]
-    return 
+    list_key = list(a_dictionary.keys())
+
+    for value_dic in list_key:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+
+    return (a_dictionary)
